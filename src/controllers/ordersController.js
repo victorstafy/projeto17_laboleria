931fdbfacacfaa0bb3ed58a6e6c9861f7a470fe7
Victor <victorstafy11@gmail.com>
2022-10-27 03:34:36 -0300

    rotas de get para orders implementadas e testadas

--- src/controllers/ordersController.js
+++ src/controllers/ordersController.js
@@ -15,6 +15,145 @@ async function postOrder(req, res) {
         console.log(error)
         return res.status(500).send(error);
     }
-} 
-  
-export {postOrder};
+}
+
+async function getOrders(req, res) {
+    const { date } = req.query;
+
+    try {
+
+        const getOrders=await connection.query(`SELECT * FROM orders`);
+        let getAllOrders;
+
+        if (getOrders.rowCount===0){
+            return res.status(404).send([]);
+        }
+
+        if (date){
+            getAllOrders = await connection.query(`
+            SELECT clients.id AS "clientId",
+            clients.name AS "clientName", 
+            clients.address AS "clientAddress",
+            clients.phone AS "clientPhone",
+            cakes.id AS "cakeId",
+            cakes.name AS "cakeName", 
+            cakes.price AS "cakePrice",
+            cakes.description AS "cakeDescription",
+            cakes.image AS "cakeImage",
+            orders.id AS "orderId", 
+            orders."createdAt", orders."quantity",
+            orders."totalPrice" FROM clients 
+            JOIN orders ON  clients.id=orders."clientId"
+            JOIN cakes ON  cakes.id=orders."cakeId"
+            WHERE DATE(orders."createdAt")=$1
+            ORDER BY orders.id ASC; 
+            `,[date])
+        }
+        else {
+            getAllOrders = await connection.query(`
+            SELECT clients.id AS "clientId",
+            clients.name AS "clientName", 
+            clients.address AS "clientAddress",
+            clients.phone AS "clientPhone",
+            cakes.id AS "cakeId",
+            cakes.name AS "cakeName", 
+            cakes.price AS "cakePrice",
+            cakes.description AS "cakeDescription",
+            cakes.image AS "cakeImage",
+            orders.id AS "orderId", 
+            orders."createdAt", orders."quantity",
+            orders."totalPrice" FROM clients 
+            JOIN orders ON  clients.id=orders."clientId"
+            JOIN cakes ON  cakes.id=orders."cakeId"
+            `)
+        }
+
+        const getAllOrdersArray=getAllOrders.rows;
+
+        const ordersArray=getAllOrdersArray.map( (order) =>({
+            client: {
+                id: order.clientId,
+                name: order.clientName,
+                address: order.clientAddress,
+                phone: order.clientPhone
+            },
+            cake: {
+                id: order.cakeId,
+                name: order.cakeName,
+                price: order.cakePrice,
+                description: order.cakeDescription,
+                image: order.cakeImage
+            },
+            orderId: order.orderId,
+            createdAt: order.createdAt,
+            quantity: order.quantity,
+            totalPrice: order.totalPrice
+        })
+        )
+        
+
+        return res.status(200).send(ordersArray);
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send(error);
+    }
+}
+
+async function getOrderById(req, res) {
+    const  paramsObj = req.params;
+    const orderId=paramsObj.id;
+
+    try {
+        const searchOrederId=await connection.query(`SELECT * FROM orders WHERE id=$1`,[orderId]);
+        let getCurrentOrder;
+
+        if (searchOrederId.rowCount===0){
+            return res.sendStatus(404);
+        }
+
+        getCurrentOrder = await connection.query(`
+        SELECT clients.id AS "clientId",
+        clients.name AS "clientName", 
+        clients.address AS "clientAddress",
+        clients.phone AS "clientPhone",
+        cakes.id AS "cakeId",
+        cakes.name AS "cakeName", 
+        cakes.price AS "cakePrice",
+        cakes.description AS "cakeDescription",
+        cakes.image AS "cakeImage",
+        orders.id AS "orderId", 
+        orders."createdAt", orders."quantity",
+        orders."totalPrice" FROM clients 
+        JOIN orders ON  clients.id=orders."clientId"
+        JOIN cakes ON  cakes.id=orders."cakeId"
+        WHERE orders.id=$1
+        `,[orderId])
+
+        const currentOrder={
+            client: {
+                id: getCurrentOrder.rows[0].clientId,
+                name: getCurrentOrder.rows[0].clientName,
+                address: getCurrentOrder.rows[0].clientAddress,
+                phone: getCurrentOrder.rows[0].clientPhone
+            },
+            cake: {
+                id: getCurrentOrder.rows[0].cakeId,
+                name: getCurrentOrder.rows[0].cakeName,
+                price: getCurrentOrder.rows[0].cakePrice,
+                description: getCurrentOrder.rows[0].cakeDescription,
+                image: getCurrentOrder.rows[0].cakeImage
+            },
+            orderId: getCurrentOrder.rows[0].orderId,
+            createdAt: getCurrentOrder.rows[0].createdAt,
+            quantity: getCurrentOrder.rows[0].quantity,
+            totalPrice: getCurrentOrder.rows[0].totalPrice
+        };
+        
+        return res.status(200).send(currentOrder);
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send(error);
+    }
+}
+
+export {postOrder,getOrders,getOrderById};
--- src/routes/ordersRoute.js
+++ src/routes/ordersRoute.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { postOrder } from "../controllers/ordersController.js";
+import { postOrder, getOrders, getOrderById } from "../controllers/ordersController.js";
 import { validOrder } from "../middlewares/ordersMiddleware.js";
 
 const orderRouter = Router();
 
 orderRouter.post('/order', validOrder, postOrder);
-// router.get('/orders', middlewares.orders, controllers.orders);
-// router.get('/orders/:id', middlewares.orders, controllers.orders);
+orderRouter.get('/orders', getOrders);
+orderRouter.get('/orders/:id', getOrderById);
 
 export default orderRouter;
